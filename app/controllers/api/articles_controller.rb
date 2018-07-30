7f5a10854b7b395516bf3ea659b9cfebee1db372
Grant Kazan <grantkazan@gmail.com>
2018-07-30 17:05:57 -0500

    added login functionality

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,6 +1,12 @@
 class Api::ArticlesController < ApplicationController
+  before_action :authenticate_user
+
   def index
-    @articles = Article.all
-    render "articles.json.jbuilder"
+    if current_user
+      @articles = Article.all
+      render "articles.json.jbuilder"
+    else
+      render json: []
+    end  
   end
 end
