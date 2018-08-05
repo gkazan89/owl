de33303bf15dd16a98e0aa90b48260a9545c37f6
Grant Kazan <grantkazan@gmail.com>
2018-08-05 14:54:42 -0500

    basic loops for artcle retrieval coded

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,12 +1,28 @@
 class Api::ArticlesController < ApplicationController
   before_action :authenticate_user
 
+  # def index
+  #   response = Unirest.get(
+  #     "https://content.guardianapis.com/sport/2018/aug/01/womens-european-tour-laura-davies-golf?show-blocks=all&api-key=#{ENV["API_KEY"]}")
+  #   render json: response.body["response"]["content"]["blocks"]["body"][0]["bodyHtml"]
+  # end
+
   def index
-    response = Unirest.get(
-      "https://content.guardianapis.com/sport/2018/aug/01/womens-european-tour-laura-davies-golf?show-blocks=all&api-key=#{ENV["API_KEY"]}")
-    render json: response.body["response"]["content"]["blocks"]["body"][0]["bodyHtml"]
+    preferences = ["technology", "weather"]
+    @articles = []
+    preferences.each do |preference|
+      response = Unirest.get(
+        "https://content.guardianapis.com/#{preference}?&api-key=#{ENV["API_KEY"]}")
+      link = response.body["response"]["results"][0]["apiUrl"] 
+      link += "?show-blocks=all&api-key=#{ENV["API_KEY"]}"
+      article = Unirest.get(link)
+      # p article.body["response"]["content"]["blocks"]["body"][0]["bodyHtml"]
+      @articles << article.body["response"]["content"]["blocks"]["body"][0]["bodyHtml"]
+    end
+    render json: @articles
   end
-  
+
+
   # may not need this along with route
   def read
     @history = History.where(user_id: current_user)
--- scratch.rb
+++ scratch.rb
@@ -14,3 +14,26 @@
 p hash[:response][:content][:blocks][:body][0][:bodyHtml]
 # THIS LINE RIGHT HERE IS EVERYTHING!!!
 
+
+# Get single article api from categories
+require "Unirest"
+
+preferences = ["technology", "weather"]
+@articles = []
+
+preferences.each do |preference|
+  response = Unirest.get(
+    "https://content.guardianapis.com/#{preference}?&api-key=#{ENV["API_KEY"]}")
+  link = response.body["response"]["results"][0]["apiUrl"] 
+
+  link += "?show-blocks=all&api-key=#{ENV["API_KEY"]}"
+
+  article = Unirest.get(link)
+
+  # p article.body["response"]["content"]["blocks"]["body"][0]["bodyHtml"]
+  @articles << article.body["response"]["content"]["blocks"]["body"][0]["bodyHtml"]
+end
+
+p @articles
+# render "index.json.jbuilder" # show all @articles
+
